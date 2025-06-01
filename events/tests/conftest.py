f4f925ea0f33fba06eb88a209c8a1ce47fcf590b
Mike Shmygel <m.shmygel@gmail.com>
2025-06-01 10:24:35 +0300

    test: add tests for accounts and events apps

---
+++ events/tests/conftest.py
@@ -0,0 +1,25 @@
+import pytest
+from django.contrib.auth.models import User
+from events.models import Event
+from datetime import timedelta
+from django.utils import timezone
+
+
+@pytest.fixture
+def user(db):
+    return User.objects.create_user(
+        username="testuser",
+        password="testpass",
+        email="test@example.com"
+    )
+
+
+@pytest.fixture
+def event(user):
+    return Event.objects.create(
+        title="Sample Event",
+        description="Description",
+        date=timezone.now() + timedelta(days=1),
+        location="Kyiv",
+        organizer=user
+    )
